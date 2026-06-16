a16e4845da09accfcadf2a6b884e51d8d27a5bd2
Khanh <nguyenphanminhkhanh9a@gmail.com>
2026-06-16 15:56:48 +0700

    Fix all JS imports breaking checkout execution

--- src/booking/checkout/checkout.js
+++ src/booking/checkout/checkout.js
@@ -1,6 +1,6 @@
-import { getCheckout, saveCheckout } from '/shared/utils/storage.js';
-import { createTransaction } from '/shared/utils/paymentService.js';
-import { formatPrice } from '/explore/home-page/movieService.js';
+import { getCheckout, saveCheckout } from '../../shared/utils/storage.js';
+import { createTransaction } from '../../shared/utils/paymentService.js';
+import { formatPrice } from '../../explore/home-page/movieService.js';
 
 const COMBOS = { none: 0, single: 65000, double: 95000 };
 
@@ -59,9 +59,9 @@ function renderCheckout() {
 
   const totalEl = document.getElementById('order-total');
   const total = Number(co.total || 0);
-import { getCheckout, saveCheckout } from '/shared/utils/storage.js';
-import { createTransaction } from '/shared/utils/paymentService.js';
-import { formatPrice } from '/explore/home-page/movieService.js';
+import { getCheckout, saveCheckout } from '../../shared/utils/storage.js';
+import { createTransaction } from '../../shared/utils/paymentService.js';
+import { formatPrice } from '../../explore/home-page/movieService.js';
 
 const COMBOS = { none: 0, single: 65000, double: 95000 };
 
